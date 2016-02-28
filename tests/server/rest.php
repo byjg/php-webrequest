110ef5b82a0b61dea50798d1ca208c0137fc8604
Joao Gilberto Magalhaes <joao@byjg.com.br>
2016-02-28 00:17:05 -0300

    Changed the rest test server to suport upload files

--- tests/server/rest.php
+++ tests/server/rest.php
@@ -8,4 +8,8 @@ $result['query_string'] = $_GET;
 $result['post_string'] = $_POST;
 $result['payload'] = file_get_contents('php://input');
 
+if (count($_FILES) > 0) {
+    $result['files'] = $_FILES;
+}
+
 echo json_encode($result);
