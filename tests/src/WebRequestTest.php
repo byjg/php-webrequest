0b6007c4572101a6b5e9de2b6d463bf761dcd633
Joao Gilberto Magalhaes <joao@byjg.com.br>
2016-07-13 22:46:43 -0300

    Fix test

--- tests/src/WebRequestTest.php
+++ tests/src/WebRequestTest.php
@@ -8,7 +8,7 @@ namespace ByJG\Util;
 class WebRequestTest extends \PHPUnit_Framework_TestCase
 {
 
-    const SERVER_TEST = 'http://localhost:8080/rest.php'; // 'http://xpto.us/webrequest-test/rest.php'; 
+    const SERVER_TEST = 'http://xpto.us/webrequest-test/rest.php'; // LOCAL: 'http://localhost:8080/rest.php';
 
     /**
      * @var WebRequest
